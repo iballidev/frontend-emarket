09547ce5560906ec231eda5929b38bd7cc7c55cf
iballi2017 <iballi2017@gmail.com>
2023-06-18 20:16:37 +0100

    starter project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
